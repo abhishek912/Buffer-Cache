2a0fe8fbfd2fe4e489bc568cf02766c52bccd43c
abhishek912 <62968028+abhishek912@users.noreply.github.com>
2020-08-20 17:00:05 +0530

    driver class to create process

---
+++ driver.cpp
@@ -0,0 +1,37 @@
+#include <iostream>
+#include <dirent.h>
+
+#include "utility/buffer_cache.hpp"
+#include "utility/process.hpp"
+
+int main() {
+    // Initializing a Buffer Cache object with free list of size 2
+    // and an empty buffer pool
+    buffer_cache cache(2);
+
+    // Counts number of opened terminals, and accordingly assigns
+    // 'pid' to a particular process later on
+    DIR *dp = opendir("/dev/pts/");
+    int i = 0;
+    if (dp != nullptr) {
+        while (readdir(dp)) {
+            i++;
+        }
+        closedir(dp);
+    } else {
+        std::cerr << "Couldn't open the Directory" << std::endl;
+    }
+
+    // Generates processes, using std::thread
+    std::thread t1(process, &cache, i - 3);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::thread t2(process, &cache, i - 2);
+    std::this_thread::sleep_for(std::chrono::milliseconds(2000));
+    std::thread t3(process, &cache, i - 1);
+
+    t1.join();
+    t2.join();
+    t3.join();
+
+    return 0;
+}
